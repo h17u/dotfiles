b27a69d03a8df4d464be2eec4957f2b23ee98714
Tomonobu Higashikawa <tomonobu.higashikawa@gmail.com>
2013-08-08 22:56:42 +0900

    add modeline and mark

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # http://blog.livedoor.jp/corylvdr/archives/6574263.html
 # /usr/bin/pmset -g ps | awk '{ if (NR == 2) print "Batteries:" $2 " Status:" $3 " " $4 " " $5; }' | sed "s/;//g"
-/usr/bin/pmset -g ps | awk '{ if (NR == 2) print "" $2 }' | sed "s/;//g"
+/usr/bin/pmset -g ps | awk '{ if (NR == 2) print "⚡" $2 }' | sed "s/;//g"
+
+# vim:set et ts=2 sts=2 sw=2 tw=0 fdm=marker fenc=utf8:
--- bin/load_average.sh
+++ bin/load_average.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # uptime | awk -F\  '{print $(NF - 2),$(NF - 1),$NF}'
-uptime | awk -F\  '{print $(NF - 1)}' | sed 's/,//g'
+uptime | awk -F\  '{print "" $(NF - 1)}' | sed 's/,//g'
+
+# vim:set et ts=2 sts=2 sw=2 tw=0 fdm=marker fenc=utf8:
